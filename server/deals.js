2a8ad8749f9b1a96a0aed628d16fb5c9497b0b23
happyahluwalia <happyahluwalia@gmail.com>
2017-06-08 16:30:08 -0400

    Server serving basic public and private deals

---
+++ server/deals.js
@@ -0,0 +1,20 @@
+const publicDeals= [ {
+    id: 1,
+    name: 'tshirt',
+    description: 'GAP TShirt',
+    originalPrice: 25.0,
+    salePrice: 22.50
+}]
+
+const privateDeals= [ {
+    id: 101,
+    name: 'Tshirt',
+    description: 'Banana Republic TShirt',
+    originalPrice: 35.0,
+    salePrice: 20.50
+}]
+
+module.exports = {
+    publicDeals,
+    privateDeals
+}
--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 var express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const Deals = require('./deals');
 // Public route for deals
 app.get('/api/deals/public', function(req,res,next){
-    let deals = [];
+    let deals = Deals.publicDeals;
     res.json(deals);
 });
 
 //Private route for deals
 //TODO : Need to protect the private routes. Check for token
-app.get('/api/deals/public', function(req,res,next){
-    let privateDeals = [];
+app.get('/api/deals/private', function(req,res,next){
+    let privateDeals = Deals.privateDeals;
     res.json(privateDeals);
 });
 
 app.listen(3000, function() {
     console.log('App listening on port 3000!');
 });
-
