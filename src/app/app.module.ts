9ea0f7bcf3b9f857df3954dc134bf3cc649dea23
happyahluwalia <happyahluwalia@gmail.com>
2017-06-08 16:36:38 -0400

    Added components for deals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PublicDealsComponent } from './deals/public-deals.component';
+import { PrivateDealsComponent } from './deals/private-deals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicDealsComponent,
+    PrivateDealsComponent
   ],
   imports: [
     BrowserModule
